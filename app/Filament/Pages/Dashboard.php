4a93058462e5b1f7fc94b319e9c49daa3bcd4d57
warHanz <20330014@student.janabadra.ac.id>
2024-12-08 13:36:47 +0700

    pagination_v2

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -4,10 +4,10 @@ namespace App\Filament\Pages;
 use Filament\Pages\Dashboard as Page;
 use App\Filament\Widgets\TableOverview;
 
-class Dashboard extends Page
-{
-    protected function getWidgets(): array
-    {
-        return [TableOverview::class];
-    }
-}
+// class Dashboard extends Page
+// {
+//     protected function getWidgets(): array
+//     {
+//         return [TableOverview::class];
+//     }
+// }
